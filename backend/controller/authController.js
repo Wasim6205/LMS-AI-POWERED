46f40e9fa69ec98b89dad91ac44635f4a4f8f775
Wasim6205 <89179686+Wasim6205@users.noreply.github.com>
2025-08-29 16:20:46 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -27,8 +27,8 @@ export const signup = async (req,res) => {
         let token = await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"none",
             maxAge:7 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json(user)
@@ -51,8 +51,8 @@ export const login = async (req,res) => {
         let token = await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"none",
             maxAge:7 * 24 * 60 * 60 * 1000
         })
         return res.status(200).json(user)
@@ -148,8 +148,8 @@ export const googleAuth = async (req,res) => {
         let token = await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"none",
             maxAge:7 * 24 * 60 * 60 * 1000
         })
         return res.status(200).json(user)
